ddff7d4827bf7a6e17b2e6c01dcf242544668043
VPriyesh7 <148794265+VPriyesh7@users.noreply.github.com>
2024-01-23 12:41:12 +0000

    Add files via upload

---
+++ spring-boot-docker/src/test/java/springbootdocker/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootdocker.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
